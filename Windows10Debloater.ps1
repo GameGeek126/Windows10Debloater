3697893d41ace3e05bda2212a3d122a6b594f8c5
GameGeek126 <85988736+GameGeek126@users.noreply.github.com>
2021-09-24 12:01:00 -0700

    TEMP Variable
    Updated script to use TEMP variable for folder so that custom TEMP env: work

--- Windows10Debloater.ps1
+++ Windows10Debloater.ps1
@@ -18,7 +18,10 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 #no errors throughout
 $ErrorActionPreference = 'silentlycontinue'
 
-$DebloatFolder = "C:\Temp\Windows10Debloater"
+#This allows it so that if you have your TEMP folder in a different spot, it can work
+$DebloatFolder = "$env:TEMP\Windows10Debloater"
+
+#New-Item -Path "$env:TEMP\" -Name "HawkPath" -ItemType "directory"
 If (Test-Path $DebloatFolder) {
     Write-Output "$DebloatFolder exists. Skipping."
 }
